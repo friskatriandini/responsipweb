642a4dd9da1a21bee311f714c5cb10a82df81bee
unknown <friskatriandini653@gmail.com>
2022-07-21 17:53:43 +0700

    first commit

---
+++ jarak.php
@@ -0,0 +1,14 @@
+<?php
+		$inama				= $_POST['inama'];
+		$iasal	 			= $_POST['iasal'];
+		$itujuan			= $_POST['itujuan'];
+		$ijumlah			= $_POST['ijumlah'];
+		$otiket     		= $_POST['otiket'];
+		$osub   			= $_POST['osub'];
+
+	$fp = fopen("jaskim.txt", "a+");
+	fputs($fp,"$inama|$iasal|$itujuan|$ijumlah|$otiket|$osub\n");
+	fclose($fp);
+
+	echo "Terimakasih, Rincian Format Anda Telah Di Cetak Pada File txt";
+	?>
